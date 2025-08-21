42e33a336b98e68579112a7be908a56a418d3068
Nihal Rajak <nihal24rajak@gmail.com>
2025-08-21 17:02:28 +0530

    cooking

--- programs/calculator/src/lib.rs
+++ programs/calculator/src/lib.rs
@@ -1,89 +1,62 @@
 use anchor_lang::prelude::*;
 
-declare_id!("ReplaceWithYourProgramId");
-
-#[account]
-pub struct NewAccount {
-    pub authority: Pubkey,
-    pub data: u32,
+#[derive(Debug)]
+struct Rect {
+    width: u32,
+    height: u32,
 }
 
-#[error_code]
-pub enum ErrorCode {
-    #[msg("Arithmetic overflow or underflow")]
-    MathError,
-    #[msg("Only the authority may modify this account")]
-    Unauthorized,
+impl Rect {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
+declare_id!("");
 
 #[program]
 pub mod anchor_calculator {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        let acct = &mut ctx.accounts.new_account;
-        acct.authority = ctx.accounts.signer.key();
-        acct.data = 1;
-
-        let rect = Rect { width: 10, height: 20 };
-        msg!("Rect: {:?}", rect);
-        msg!("Area: {}", rect.area());
+        ctx.accounts.new_account.data = 1;
+        let rect = Rect {
+            width: 10,
+            height: 20,
+        };
+        println!("Rect: {:?}", rect);
         Ok(())
     }
 
-    pub fn double(ctx: Context<Modify>) -> Result<()> {
-        only_authority(&ctx.accounts.account, &ctx.accounts.authority)?;
-        let x = ctx.accounts.account.data.checked_mul(2).ok_or(ErrorCode::MathError)?;
-        ctx.accounts.account.data = x;
+    pub fn double(ctx: Context<Double>) -> Result<()> {
+        ctx.accounts.account.data = ctx.accounts.account.data * 2;
+        Ok(())
+    }
+    
+    pub fn halve(ctx: Context<Halve>) -> Result<()> {
+        ctx.accounts.account.data = ctx.accounts.account.data / 2;
         Ok(())
     }
 
-    pub fn halve(ctx: Context<Modify>) -> Result<()> {
-        only_authority(&ctx.accounts.account, &ctx.accounts.authority)?;
-        ctx.accounts.account.data /= 2;
+    pub fn add(ctx: Context<Add>, amount: u32) -> Result<()> {
+        ctx.accounts.account.data = ctx.accounts.account.data + amount;
         Ok(())
     }
-
-    pub fn add(ctx: Context<ModifyWithAmount>, amount: u32) -> Result<()> {
-        only_authority(&ctx.accounts.account, &ctx.accounts.authority)?;
-        let x = ctx.accounts.account.data.checked_add(amount).ok_or(ErrorCode::MathError)?;
-        ctx.accounts.account.data = x;
-        Ok(())
-    }
-
-    pub fn sub(ctx: Context<ModifyWithAmount>, amount: u32) -> Result<()> {
-        only_authority(&ctx.accounts.account, &ctx.accounts.authority)?;
-        let x = ctx.accounts.account.data.checked_sub(amount).ok_or(ErrorCode::MathError)?;
-        ctx.accounts.account.data = x;
+    
+    pub fn sub(ctx: Context<Sub>, amount: u32) -> Result<()> {
+        ctx.accounts.account.data = ctx.accounts.account.data - amount;
         Ok(())
     }
 }
 
-fn only_authority(acct: &Account<NewAccount>, signer: &Signer) -> Result<()> {
-    require_keys_eq!(acct.authority, signer.key(), ErrorCode::Unauthorized);
-    Ok(())
-}
 
-#[derive(Debug)]
-struct Rect {
-    width: u32,
-    height: u32,
-}
-impl Rect {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
+#[account]
+pub struct NewAccount {
+    data: u32,
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(
-        init,
-        payer = signer,
-        space = 8 + 32 + 4,
-        seeds = [b"calc", signer.key().as_ref()],
-        bump
-    )]
+    #[account(init, payer = signer, space = 8 + 4)]
     pub new_account: Account<'info, NewAccount>,
     #[account(mut)]
     pub signer: Signer<'info>,
@@ -91,25 +64,29 @@ pub struct Initialize<'info> {
 }
 
 #[derive(Accounts)]
-pub struct Modify<'info> {
-    #[account(
-        mut,
-        seeds = [b"calc", authority.key().as_ref()],
-        bump,
-        has_one = authority
-    )]
+pub struct Double<'info> {
+    #[account(mut)]
     pub account: Account<'info, NewAccount>,
-    pub authority: Signer<'info>,
+    pub signer: Signer<'info>,
 }
 
 #[derive(Accounts)]
-pub struct ModifyWithAmount<'info> {
-    #[account(
-        mut,
-        seeds = [b"calc", authority.key().as_ref()],
-        bump,
-        has_one = authority
-    )]
+pub struct Halve<'info> {
+    #[account(mut)]
     pub account: Account<'info, NewAccount>,
-    pub authority: Signer<'info>,
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Add<'info> {
+    #[account(mut)]
+    pub account: Account<'info, NewAccount>,
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Sub<'info> {
+    #[account(mut)]
+    pub account: Account<'info, NewAccount>,
+    pub signer: Signer<'info>,
 }
