a439f90087e15fbc2869a2648af0a5959068dc43
Nihal Rajak <nihal24rajak@gmail.com>
2025-08-22 11:51:08 +0530

    cokking

--- programs/cpi-contract/src/lib.rs
+++ programs/cpi-contract/src/lib.rs
@@ -1,16 +1,30 @@
 use anchor_lang::prelude::*;
-
+use anchor_lang::system_program::{transfer ,Transfer};
 declare_id!("6Cn3NiqpBqFavRqahmvqpsWcgmFCi72cD3wNaY4Y9bNC");
 
 #[program]
 pub mod cpi_contract {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn solana_transfer(ctx: Context<Initialize>, amount :u32) -> Result<()> {
+        let from_pub= ctx.accounts.sender.to_account_info();
+        let to_pubkey= ctx.accounts.receiver.to_account_info(); 
+        let program_id = ctx.accounts.system_program.to_account_info();
+         
+        let cpi_context = CpiContext::new(program_id, Transfer{
+        from: from_pub,
+        to: to_pubkey
+        },);
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    sender : Signer<'info>,
+    #[account(mut)]
+    receiver : Signer<'info>,
+    system_program: Program<'info, System>
+}
